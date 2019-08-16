a2b4f5f372d6a1c5dfeb9adb83c985633681ff2a
Sworen <jerry.sworen@imetro.org>
2019-08-16 13:27:18 -0400

    commit

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,26 @@
+$(document).ready(function(){
+  getWeather();
+})
+
+function getWeather(searhQuery) {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&APPID="+apiKey;
+
+  $(".city").text("");
+  $(".temp").text("");
+
+  $.ajax(url,{success:function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }, error: function(error){
+    $(".error-message").text("An error occured");
+  }})
+}
+
+function searchWeb() {
+  var searchQuery= $(".search").val();
+  getWeather(searchQuery);
+}
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
@@ -5,5 +28,27 @@ function showPicture(){
   $("p").html("High five! You're building your first web app!");
 
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
+  
+}
+
+function handleSignIn() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
   
 }
